0157e5a06a2af09535f524c7a1af20feef99ad06
Pau Andrio <andriopau@gmail.com>
2018-06-12 13:32:57 +0200

    Adding BB Genrest NOT testedgit add .!

--- gromacs/genrestr.py
+++ gromacs/genrestr.py
@@ -1,127 +1,105 @@
 #!/usr/bin/env python
-
-"""Python wrapper module for the GROMACS genrestr module
-"""
-import sys
+import argparse
+from biobb_common.configuration import  settings
+from biobb_common.tools import file_utils as fu
+from biobb_common.command_wrapper import cmd_wrapper
 import re
-import json
-import os
-import configuration.settings as settings
-from command_wrapper import cmd_wrapper
-from tools import file_utils as fu
-
 
 class Genrestr(object):
     """Wrapper class for the GROMACS genrestr module.
 
     Args:
-        input_structure_path (str): Path to the input structure PDB/GRO/TPR file.
-        input_ndx_path (str): Path to the input index NDX file.
+        input_structure_path (str): Path to the input structure PDB, GRO or TPR format.
+        input_ndx_path (str): Path to the input GROMACS index file, NDX format.
         input_top_zip_path (str): Path the input TOP topology in zip format.
         output_top_zip_path (str): Path the output TOP topology in zip format.
         properties (dic):
-            | **output_top_path** (*str*): Path the output TOP file.
-            | **output_itp_path** (*str*): Path to the output include for topology ITP file.
-            | **force_constants** (*float[3]*): Array of three floats defining the force constants
+            | - **output_top_path** (*str*) - ("restrain.top") Path the output TOP file.
+            | - **output_itp_path** (*str*) - ("restrain.itp") Path to the output include for topology ITP file.
+            | - **force_constants** (*str*) - ("500 500 500") Array of three floats defining the force constants
+            | - **gmx_path** (*str*) - ("gmx") Path to the GROMACS executable binary.
+
     """
 
     def __init__(self, input_structure_path, input_ndx_path, input_top_zip_path,
                  output_top_zip_path, properties, **kwargs):
-        if isinstance(properties, basestring):
-            properties=json.loads(properties)
         self.input_structure_path = input_structure_path
         self.input_ndx_path = input_ndx_path
         self.input_top_zip_path = input_top_zip_path
         self.output_top_zip_path = output_top_zip_path
+        # Properties specific for BB
         self.output_itp_path = properties.get('output_itp_path','restrain.itp')
         self.output_top_path = properties.get('output_top_path','restrain.top')
-        self.force_constants = properties.get('force_constants','500 500 500')
+        self.force_constants = str(properties.get('force_constants','500 500 500'))
         self.restricted_group = properties.get('restricted_group', 'system')
-        self.gmx_path = properties.get('gmx_path',None)
-        self.mutation = properties.get('mutation',None)
+        # Common in all BB
+        self.gmx_path = properties.get('gmx_path','gmx')
+        self.global_log= properties.get('global_log', None)
+        self.prefix = properties.get('prefix',None)
         self.step = properties.get('step',None)
         self.path = properties.get('path','')
-        self.mpirun = properties.get('mpirun',False)
-        self.mpirun_np = properties.get('mpirun_np',None)
 
     def launch(self):
-        """Launches the execution of the GROMACS pdb2gmx module.
+        """Launches the execution of the GROMACS genrestr module.
         """
-        out_log, err_log = fu.get_logs(path=self.path, mutation=self.mutation, step=self.step)
-        self.output_top_path = fu.add_step_mutation_path_to_name(self.output_top_path, self.step, self.mutation)
-        self.output_itp_path = fu.add_step_mutation_path_to_name(self.output_itp_path, self.step, self.mutation)
+        out_log, err_log = fu.get_logs(path=self.path, prefix=self.prefix, step=self.step)
+        self.output_top_path = fu.create_name(path=self.path, prefix=self.prefix, step=self.step, name=self.output_top_path)
+        self.output_itp_path = fu.create_name(path=self.path, prefix=self.prefix, step=self.step, name=self.output_itp_path)
 
-        gmx = "gmx" if self.gmx_path is None else self.gmx_path
-        cmd = [gmx, "genrestr", "-f", self.input_structure_path,
-               "-n", self.input_ndx_path, "-o", self.output_itp_path,
+        out_log.info('Adding restraints for atoms in group: '+self.restricted_group+' using a force constant of: '+self.force_constants)
+        if self.global_log:
+            self.global_log.info(fu.get_logs_prefix()+'Adding restraints for atoms in group: '+self.restricted_group+' using a force constant of: '+self.force_constants)
+
+        cmd = ['echo', '\"'+self.restricted_group+'\"', '|',
+               self.gmx_path, "genrestr",
+               "-f", self.input_structure_path,
+               "-n", self.input_ndx_path,
+               "-o", self.output_itp_path,
                "-fc", self.force_constants]
 
-        if self.mpirun_np is not None:
-            cmd.insert(0, str(self.mpirun_np))
-            cmd.insert(0, '-np')
-        if self.mpirun:
-            cmd.insert(0, 'mpirun')
-
-        if self.mpirun:
-            cmd.append('<<<')
-            cmd.append('\"'+self.restricted_group+'\"')
-        else:
-            cmd.insert(0, '|')
-            cmd.insert(0, '\"'+self.restricted_group+'\"')
-            cmd.insert(0, 'echo')
-
-        command = cmd_wrapper.CmdWrapper(cmd, out_log, err_log)
+        command = cmd_wrapper.CmdWrapper(cmd, out_log, err_log, self.global_log)
         returncode = command.launch()
 
-        fu.unzip_top(zip_file=self.input_top_zip_path, top_file=self.output_top_path)
-        out_log.info('Unzip: '+ self.input_top_zip_path + ' to: '+self.output_top_path)
+        # Unzip topology to topology_out
+        fu.unzip_top(zip_file=self.input_top_zip_path, top_file=self.output_top_path, out_log=out_log)
+        # Find ITP file name in the topology
         with open(self.output_top_path, 'r') as fin:
             for line in fin:
                 if line.startswith('#ifdef POSRES'):
                     itp_name = re.findall('"([^"]*)"',fin.next())[0]
-                    out_log.debug('itp_name: '+itp_name)
                     break
-
-        # with open(self.output_top_path, 'r') as fin:
-        #     data = fin.read().splitlines(True)
-        #     index = data.index('#ifdef POSRES\n')
-        #     data[index+2] = 'system\n'
-        #     data.insert(index, '\n')
-        #     data.insert(index, '#endif\n')
-        #     data.insert(index, '#include "'+self.output_itp_path+'"\n')
-        #     data.insert(index, '#ifdef CUSTOM_POSRES\n')
-        #     data.insert(index, '; Include Position restraint file\n')
-        #     # data.insert(index, '#include "'+self.output_itp_path+'"\n')
-        #     # data.insert(index, '; Include genrestr generated itp\n')
-        # with open(self.output_top_path, 'w') as fout:
-        #     fout.writelines(data)
-
+        # Overwrite the content of the ITP file in the topology with the
+        # content of the ITP file created with genrest.
         with open(self.output_itp_path, 'r') as fin:
             data = fin.read().splitlines(True)
-            # data.insert(0, '\n')
-            # data.insert(0, 'system    3\n')
-            # data.insert(0, ';Name    nrexcl\n')
-            # data.insert(0, '[ system ]\n')
         with open(itp_name, 'w') as fout:
             fout.writelines(data)
+        # Remove the ITP file created with genrest.
         os.remove(self.output_itp_path)
-
-
         # zip topology
-        fu.zip_top(self.output_top_path, self.output_top_zip_path, remove_files=False)
-        out_log.info('Zip: '+ self.output_top_path +' to: '+ self.output_top_zip_path)
+        fu.zip_top(zip_file=self.output_top_zip_path, out_log=out_log)
 
         return returncode
-#Creating a main function to be compatible with CWL
+
 def main():
-    system=sys.argv[1]
-    step=sys.argv[2]
-    properties_file=sys.argv[3]
-    prop = settings.YamlReader(properties_file, system).get_prop_dic()[step]
-    Pdb2gmx(input_structure_pdb_path=sys.argv[4],
-            output_gro_path=sys.argv[5],
-            output_top_zip_path=sys.argv[6],
-            properties=prop).launch()
+    parser = argparse.ArgumentParser(description="Wrapper for the GROMACS genion module.")
+    parser.add_argument('--conf_file', required=True)
+    parser.add_argument('--system', required=True)
+    parser.add_argument('--step', required=True)
+
+    #Specific args of each building block
+    parser.add_argument('--input_structure_path', required=True)
+    parser.add_argument('--input_ndx_path', required=True)
+    parser.add_argument('--input_top_zip_path', required=True)
+    parser.add_argument('--output_top_zip_path', required=True)
+    ####
+
+    args = parser.parse_args()
+    properties = settings.YamlReader(conf_file_path=args.conf_file, system=args.system).get_prop_dic()[args.step]
+
+    #Specific call of each building block
+    Genrestr(input_structure_path=args.input_structure_path, input_ndx_path=args.input_ndx_path, input_top_zip_path=args.input_top_zip_path, output_top_zip_path=args.output_top_zip_path, properties=properties).launch()
+    ####
 
 if __name__ == '__main__':
     main()
