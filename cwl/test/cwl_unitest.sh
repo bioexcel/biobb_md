17050b32175b3fed62b842482264c115a65a2547
Pau Andrio <andriopau@gmail.com>
2018-07-09 18:00:43 +0200

    Add CWL definition of rms bb

--- cwl/test/cwl_unitest.sh
+++ cwl/test/cwl_unitest.sh
@@ -8,3 +8,4 @@ cwltool $BIOBB_MD/cwl/gromacs/grompp.cwl $BIOBB_MD/cwl/test/gromacs/grompp.yml
 cwltool $BIOBB_MD/cwl/gromacs/mdrun.cwl $BIOBB_MD/cwl/test/gromacs/mdrun.yml
 cwltool $BIOBB_MD/cwl/gromacs/solvate.cwl $BIOBB_MD/cwl/test/gromacs/solvate.yml
 cwltool $BIOBB_MD/cwl/gromacs/make_ndx.cwl $BIOBB_MD/cwl/test/gromacs/make_ndx.yml
+cwltool $BIOBB_MD/cwl/gromacs/rms.cwl $BIOBB_MD/cwl/test/gromacs/rms.yml
