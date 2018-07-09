dfb91c228e00b8bd6269c56d373e97ecc8a80825
Pau Andrio <andriopau@gmail.com>
2018-07-09 12:26:55 +0200

    Adding CWL definition of Genrestr bb

--- cwl/test/cwl_unitest.sh
+++ cwl/test/cwl_unitest.sh
@@ -3,3 +3,4 @@ cwltool $BIOBB_MD/cwl/gromacs/pdb2gmx.cwl $BIOBB_MD/cwl/test/gromacs/pdb2gmx.yml
 cwltool $BIOBB_MD/cwl/gromacs/editconf.cwl $BIOBB_MD/cwl/test/gromacs/editconf.yml
 cwltool $BIOBB_MD/cwl/gromacs/genion.cwl $BIOBB_MD/cwl/test/gromacs/genion.yml
 cwltool $BIOBB_MD/cwl/gromacs/cluster.cwl $BIOBB_MD/cwl/test/gromacs/cluster.yml
+cwltool $BIOBB_MD/cwl/gromacs/genrestr.cwl $BIOBB_MD/cwl/test/gromacs/genrestr.yml
