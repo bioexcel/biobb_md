be4978c96af732b0a23467cbd140821922520d54
Pau Andrio <andriopau@gmail.com>
2018-09-14 13:59:03 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_md",
-    version="0.0.1",
+    version="0.0.2",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb_md is the Biobb module collection to perform molecular dynamics simulations.",
