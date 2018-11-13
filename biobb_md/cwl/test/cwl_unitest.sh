454dcd2a1054480880c0f3eb77da75c83e061f0a
Pau Andrio <andriopau@gmail.com>
2018-11-13 20:33:09 +0100

    Adding references to biobb_adapters to fix the links in the D2.4

---
+++ biobb_md/cwl/test/cwl_unitest.sh
@@ -0,0 +1,2 @@
+This file has been moved to the new biobb_adapters repository.
+Please refer to: https://github.com/bioexcel/biobb_adapters/tree/master/biobb_adapters/biobb_md/cwl/test/cwl_unitest.sh
