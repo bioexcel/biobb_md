f557418663db1f994e8a946552a088f2081de349
Pau Andrio <andriopau@gmail.com>
2019-05-14 12:04:19 +0200

    May 2019 Release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_md",
-    version="0.1.5",
+    version="1.1.6",
     author="Biobb developers",
     author_email="pau.andrio@bsc.es",
     description="Biobb_md is the Biobb module collection to perform molecular dynamics simulations.",
@@ -18,7 +18,7 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
-    install_requires=['biobb_common>=0.1.2'],
+    install_requires=['biobb_common==1.1.6'],
     python_requires='>=3',
     classifiers=(
         "Development Status :: 3 - Alpha",
