37f020918e68a5583428dfb06df89e28021a09f2
Pau Andrio <andriopau@gmail.com>
2019-11-19 18:02:01 +0100

    Adding container to make_ndx

--- biobb_md/gromacs/make_ndx.py
+++ biobb_md/gromacs/make_ndx.py
@@ -5,37 +5,53 @@ import os
 import shutil
 import argparse
 import pathlib as pl
-from biobb_common.configuration import  settings
+from biobb_common.configuration import settings
 from biobb_common.tools import file_utils as fu
 from biobb_common.tools.file_utils import launchlogger
 from biobb_common.command_wrapper import cmd_wrapper
 from biobb_md.gromacs.common import get_gromacs_version
 from biobb_md.gromacs.common import GromacsVersionError
 
-class MakeNdx():
+
+class MakeNdx:
     """Wrapper of the GROMACS make_ndx (http://manual.gromacs.org/current/onlinehelp/gmx-make_ndx.html) module.
 
     Args:
         input_structure_path (str): Path to the input GRO/PDB/TPR file.
         output_ndx_path (str): Path to the output index NDX file.
+        input_ndx_path (str)[Optional]: Path to the input index NDX file.
         properties (dic):
-            | - **selection** (*str*) - ("a CA C N O") Heavy atoms. Atom selection string.
-            | - **gmx_path** (*str*) - ("gmx") Path to the GROMACS executable binary.
-            | - **remove_tmp** (*bool*) - (True) [WF property] Remove temporal files.
-            | - **restart** (*bool*) - (False) [WF property] Do not execute if output files exist.
+            * **selection** (*str*) - ("a CA C N O") Heavy atoms. Atom selection string.
+            * **gmx_path** (*str*) - ("gmx") Path to the GROMACS executable binary.
+            * **remove_tmp** (*bool*) - (True) [WF property] Remove temporal files.
+            * **restart** (*bool*) - (False) [WF property] Do not execute if output files exist.
+            * **container_path** (*string*) - (None)  Path to the binary executable of your container.
+            * **container_image** (*string*) - ("gromacs/gromacs:latest") Container Image identifier.
+            * **container_volume_path** (*string*) - ("/data") Path to an internal directory in the container.
+            * **container_working_dir** (*string*) - (None) Path to the internal CWD in the container.
+            * **container_user_id** (*string*) - (None) User number id to be mapped inside the container.
+            * **container_shell_path** (*string*) - ("/bin/bash") Path to the binary executable of the container shell.
     """
 
     def __init__(self, input_structure_path, output_ndx_path, input_ndx_path=None, properties=None, **kwargs):
         properties = properties or {}
 
         # Input/Output files
-        self.input_structure_path = input_structure_path
-        self.output_ndx_path = output_ndx_path
-        #Optional files
-        self.input_ndx_path = input_ndx_path
+        self.io_dict = {
+            "in": {"input_structure_path": input_structure_path},
+            "out": {"output_ndx_path": output_ndx_path, "input_ndx_path": input_ndx_path}
+        }
 
         # Properties specific for BB
         self.selection = properties.get('selection', "a CA C N O")
+
+        # container Specific
+        self.container_path = properties.get('container_path')
+        self.container_image = properties.get('container_image', 'gromacs/gromacs:latest')
+        self.container_volume_path = properties.get('container_volume_path', '/data')
+        self.container_working_dir = properties.get('container_working_dir')
+        self.container_user_id = properties.get('container_user_id')
+        self.container_shell_path = properties.get('container_shell_path', '/bin/bash')
 
         # Properties common in all GROMACS BB
         self.gmxlib = properties.get('gmxlib', None)
@@ -46,7 +62,7 @@ class MakeNdx():
             self.gmx_path += ' -nobackup'
         if self.gmx_nocopyright:
             self.gmx_path += ' -nocopyright'
-        if not properties.get('docker_path'):
+        if not self.container_path:
             self.gmx_version = get_gromacs_version(self.gmx_path)
 
         # Properties common in all BB
@@ -58,11 +74,6 @@ class MakeNdx():
         self.remove_tmp = properties.get('remove_tmp', True)
         self.restart = properties.get('restart', False)
 
-        # Docker Specific
-        self.docker_path = properties.get('docker_path')
-        self.docker_image = properties.get('docker_image', 'mmbirb/pmx')
-        self.docker_volume_path = properties.get('docker_volume_path', '/inout')
-
         # Check the properties
         fu.check_properties(self, properties)
 
@@ -71,78 +82,68 @@ class MakeNdx():
         """Launches the execution of the GROMACS make_ndx module."""
         tmp_files = []
 
-
         # Get local loggers from launchlogger decorator
         out_log = getattr(self, 'out_log', None)
         err_log = getattr(self, 'err_log', None)
 
-        #Check GROMACS version
-        if not self.docker_path:
+        # Check GROMACS version
+        if not self.container_path:
             if self.gmx_version < 512:
                 raise GromacsVersionError("Gromacs version should be 5.1.2 or newer %d detected" % self.gmx_version)
             fu.log("GROMACS %s %d version detected" % (self.__class__.__name__, self.gmx_version), out_log)
 
-        #Restart if needed
+        # Restart if needed
         if self.restart:
-            output_file_list = [self.output_ndx_path]
-            if fu.check_complete_files(output_file_list):
+            if fu.check_complete_files(self.io_dict["out"].values()):
                 fu.log('Restart is enabled, this step: %s will the skipped' % self.step, out_log, self.global_log)
                 return 0
 
-        cmd_docker = []
-        cmd = ['echo', '\"'+self.selection+' \n q'+'\"', '|',
+        container_io_dict = fu.copy_to_container(self.container_path, self.container_volume_path, self.io_dict)
+
+        cmd = ['echo', '\'' + self.selection + ' \n q' + '\'', '|',
                self.gmx_path, 'make_ndx',
-               '-f', self.input_structure_path,
-               '-o', self.output_ndx_path]
+               '-f', container_io_dict["in"]["input_structure_path"],
+               '-o', container_io_dict["out"]["output_ndx_path"]
+               ]
 
-        if self.docker_path:
-            fu.log('Docker execution enabled', out_log)
-            unique_dir = pl.Path((fu.create_unique_dir())).resolve()
-            shutil.copy2(self.input_structure_path, unique_dir)
-            docker_input_structure_path = pl.Path(self.docker_volume_path, pl.Path(self.input_structure_path).name)
-            docker_output_ndx_path = pl.Path(self.docker_volume_path, pl.Path(self.output_ndx_path).name)
-            cmd_docker = [self.docker_path, 'run',
-                          '-v', str(unique_dir)+':'+str(self.docker_volume_path),
-                          '--user', str(os.getuid()),
-                          self.docker_image]
-
-            cmd = ['echo', '\"'+self.selection+' \n q'+'\"', '|',
-                   self.gmx_path, 'make_ndx',
-                   '-f', docker_input_structure_path,
-                   '-o', docker_output_ndx_path]
-
-        if self.input_ndx_path and pl.Path(self.input_ndx_path).exists():
+        if container_io_dict["in"].get("input_ndx_path") and pl.Path(
+                container_io_dict["in"].get("input_ndx_path")).exists():
             cmd.append('-n')
-            if self.docker_path:
-                shutil.copy2(self.input_ndx_path, unique_dir)
-                cmd.append(str(pl.Path(self.docker_volume_path, pl.Path(self.input_ndx_path).name)))
-            else:
-                cmd.append(self.input_ndx_path)
-
-        if self.docker_path:
-            cmd = ['"' + " ".join(cmd) + '"']
-            cmd_docker.extend(['/bin/bash', '-c'])
+            cmd.append(container_io_dict["in"].get("input_ndx_path"))
 
         new_env = None
         if self.gmxlib:
             new_env = os.environ.copy()
             new_env['GMXLIB'] = self.gmxlib
 
+        cmd = fu.create_cmd_line(cmd, container_path=self.container_path,
+                                 host_volume=container_io_dict.get("unique_dir"),
+                                 container_volume=self.container_volume_path,
+                                 container_working_dir=self.container_working_dir,
+                                 container_user_uid=self.container_user_id,
+                                 container_shell_path=self.container_shell_path,
+                                 container_image=self.container_image,
+                                 out_log=out_log, global_log=self.global_log)
         returncode = cmd_wrapper.CmdWrapper(cmd, out_log, err_log, self.global_log, new_env).launch()
+        fu.copy_to_host(self.container_path, container_io_dict, self.io_dict)
 
-        if self.docker_path:
-            tmp_files.append(unique_dir)
-            shutil.copy2(pl.Path(unique_dir, os.Path(self.output_gro_path).name), self.output_gro_path)
+        tmp_files.append(container_io_dict.get("unique_dir"))
+        if self.remove_tmp:
+            fu.rm_file_list(tmp_files, out_log=out_log)
 
         return returncode
 
-def main():
-    parser = argparse.ArgumentParser(description="Wrapper for the GROMACS make_ndx module.", formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999))
-    parser.add_argument('-c', '--config', required=False, help="This file can be a YAML file, JSON file or JSON string")
-    parser.add_argument('--system', required=False, help="Check 'https://biobb-common.readthedocs.io/en/latest/system_step.html' for help")
-    parser.add_argument('--step', required=False, help="Check 'https://biobb-common.readthedocs.io/en/latest/system_step.html' for help")
 
-    #Specific args of each building block
+def main():
+    parser = argparse.ArgumentParser(description="Wrapper for the GROMACS make_ndx module.",
+                                     formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999))
+    parser.add_argument('-c', '--config', required=False, help="This file can be a YAML file, JSON file or JSON string")
+    parser.add_argument('--system', required=False,
+                        help="Check 'https://biobb-common.readthedocs.io/en/latest/system_step.html' for help")
+    parser.add_argument('--step', required=False,
+                        help="Check 'https://biobb-common.readthedocs.io/en/latest/system_step.html' for help")
+
+    # Specific args of each building block
     required_args = parser.add_argument_group('required arguments')
     required_args.add_argument('--input_structure_path', required=True)
     required_args.add_argument('--output_ndx_path', required=True)
@@ -154,8 +155,10 @@ def main():
     if args.step:
         properties = properties[args.step]
 
-    #Specific call of each building block
-    MakeNdx(input_structure_path=args.input_structure_path, output_ndx_path=args.output_ndx_path, input_ndx_path=args.input_ndx_path, properties=properties).launch()
+    # Specific call of each building block
+    MakeNdx(input_structure_path=args.input_structure_path, output_ndx_path=args.output_ndx_path,
+            input_ndx_path=args.input_ndx_path, properties=properties).launch()
+
 
 if __name__ == '__main__':
     main()
