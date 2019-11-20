62e4c1974c09272c95133f56a8e645ac04874318
Pau Andrio <andriopau@gmail.com>
2019-11-20 18:12:11 +0100

    Fixing tests

--- biobb_md/test/unitests/test_gromacs/test_genion_container.py
+++ biobb_md/test/unitests/test_gromacs/test_genion_container.py
@@ -6,8 +6,8 @@ class TestGenionDocker():
         fx.test_setup(self, 'genion_container')
 
     def tearDown(self):
-        pass
-        #fx.test_teardown(self)
+        #pass
+        fx.test_teardown(self)
 
     def test_genion_docker(self):
         returncode= Genion(properties=self.properties, **self.paths).launch()
--- biobb_md/test/unitests/test_gromacs/test_solvate_container.py
+++ biobb_md/test/unitests/test_gromacs/test_solvate_container.py
@@ -6,8 +6,8 @@ class TestSolvateDocker():
         fx.test_setup(self,'solvate_container')
 
     def tearDown(self):
-        pass
-        #fx.test_teardown(self)
+        #pass
+        fx.test_teardown(self)
 
     def test_solvate(self):
         returncode = Solvate(properties=self.properties, **self.paths).launch()
