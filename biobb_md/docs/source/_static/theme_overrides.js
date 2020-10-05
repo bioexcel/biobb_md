c3211a8d9f965b7f2c0fbae4b82d18ec53620fda
gbayarri <genis.bayarri@irbbarcelona.org>
2020-10-05 17:19:02 +0200

    Fixed logo in modules

--- biobb_md/docs/source/_static/theme_overrides.js
+++ biobb_md/docs/source/_static/theme_overrides.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	var rel_path = $('.icon.icon-home').attr('href').input.split('index.html')[0];
+	var rel_path = $('.icon.icon-home').attr('href').split('index.html')[0];
 	$('.icon.icon-home').html('<img src="' + rel_path + '_static/logo.png" class="logo" alt="Logo">');
 });
