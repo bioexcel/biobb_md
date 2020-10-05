469c8a375a4dd67cfae4e187f0d567cf9d9bb466
gbayarri <genis.bayarri@irbbarcelona.org>
2020-10-05 16:58:00 +0200

    New read the docs styles

--- biobb_md/gromacs/grompp.py
+++ biobb_md/gromacs/grompp.py
@@ -15,7 +15,7 @@ from biobb_md.gromacs.common import GromacsVersionError
 
 class Grompp:
     """
-    | **biobb_md Grompp**
+    | biobb_md Grompp
     | Wrapper of the `GROMACS grompp <http://manual.gromacs.org/current/onlinehelp/gmx-grompp.html>`_ module.
     | The GROMACS preprocessor module needs to be fed with the input system and the dynamics parameters to create a portable binary run input file TPR. The dynamics parameters are specified in the mdp section of the configuration YAML file. The parameter names and defaults are the same as the ones in the `official MDP specification <http://manual.gromacs.org/current/user-guide/mdp-options.html>`_.
 
