f5176c12178769faf2067820d1e5ef3f83bb89f2
Pau Andrio <andriopau@gmail.com>
2020-12-19 17:43:07 +0100

    docs(genion): Adding docstrings2.0 and standalone func

--- biobb_md/gromacs/genion.py
+++ biobb_md/gromacs/genion.py
@@ -14,18 +14,20 @@ from biobb_md.gromacs.common import GromacsVersionError
 
 
 class Genion:
-    """Wrapper class for the `GROMACS genion <http://manual.gromacs.org/current/onlinehelp/gmx-genion.html>`_ module.
+    """
+    | biobb_md genion
+    | Wrapper class for the `GROMACS genion <http://manual.gromacs.org/current/onlinehelp/gmx-genion.html>`_ module.
+    | The GROMACS genion module randomly replaces solvent molecules with monoatomic ions. The group of solvent molecules should be continuous and all molecules should have the same number of atoms.
 
     Args:
-        input_tpr_path (str): Path to the input portable run input TPR file. File type: input. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/data/gromacs/genion.tpr>`_. Accepted formats: tpr.
-        output_gro_path (str): Path to the input structure GRO file. File type: output. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/reference/gromacs/ref_genion.gro>`_. Accepted formats: gro.
-        input_top_zip_path (str): Path the input TOP topology in zip format. File type: input. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/data/gromacs/genion.zip>`_. Accepted formats: zip.
-        output_top_zip_path (str): Path the output topology TOP and ITP files zipball. File type: output. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/reference/gromacs/ref_genion.zip>`_. Accepted formats: zip.
-        properties (dic):
-            * **output_top_path** (*str*) - ("gio.top") Path the output topology TOP file.
+        input_tpr_path (str): Path to the input portable run input TPR file. File type: input. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/data/gromacs/genion.tpr>`_. Accepted formats: tpr (edam:format_2333).
+        output_gro_path (str): Path to the input structure GRO file. File type: output. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/reference/gromacs/ref_genion.gro>`_. Accepted formats: gro (edam:format_2033).
+        input_top_zip_path (str): Path the input TOP topology in zip format. File type: input. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/data/gromacs/genion.zip>`_. Accepted formats: zip (edam:format_3987).
+        output_top_zip_path (str): Path the output topology TOP and ITP files zipball. File type: output. `Sample file <https://github.com/bioexcel/biobb_md/raw/master/biobb_md/test/reference/gromacs/ref_genion.zip>`_. Accepted formats: zip (edam:format_3987).
+        properties (dict - Python dictionary object containing the tool parameters, not input/output files):
             * **replaced_group** (*str*) - ("SOL") Group of molecules that will be replaced by the solvent.
             * **neutral** (*bool*) - (False) Neutralize the charge of the system.
-            * **concentration** (*float*) - (0.05) Concentration of the ions in (mol/liter).
+            * **concentration** (*float*) - (0.05) [0~10|0.01] Concentration of the ions in (mol/liter).
             * **seed** (*int*) - (1993) Seed for random number generator.
             * **gmx_lib** (*str*) - (None) Path set GROMACS GMXLIB environment variable.
             * **gmx_path** (*str*) - ("gmx") Path to the GROMACS executable binary.
@@ -37,8 +39,28 @@ class Genion:
             * **container_working_dir** (*str*) - (None) Path to the internal CWD in the container.
             * **container_user_id** (*str*) - (None) User number id to be mapped inside the container.
             * **container_shell_path** (*str*) - ("/bin/bash") Path to the binary executable of the container shell.
-    """
 
+    Examples:
+        This is a use example of how to use the building block from Python::
+
+            from biobb_md.gromacs.genion import genion
+            prop = { 'concentration': 0.05,
+                     'replaced_group': 'SOL'}
+            genion(input_tpr_path='/path/to/myPortableBinaryRunInputFile.tpr',
+                   output_gro_path='/path/to/newStructure.gro',
+                   input_top_zip_path='/path/to/myTopology.zip',
+                   output_top_zip_path='/path/to/newTopology.zip',
+                   properties=prop)
+
+    Info:
+        * wrapped_software:
+            * name: GROMACS Genion
+            * version: >5.1
+            * license: LGPL 2.1
+        * ontology:
+            * name: EDAM
+            * schema: http://edamontology.org/EDAM.owl
+    """
     def __init__(self, input_tpr_path: str, output_gro_path: str, input_top_zip_path: str,
                  output_top_zip_path: str, properties: dict = None, **kwargs) -> None:
         properties = properties or {}
@@ -52,7 +74,7 @@ class Genion:
         self.input_top_zip_path = input_top_zip_path
 
         # Properties specific for BB
-        self.output_top_path = properties.get('output_top_path', 'gio.top')
+        self.output_top_path = properties.get('output_top_path', 'gio.top')  # Not in documentation for clarity
         self.replaced_group = properties.get('replaced_group', 'SOL')
         self.neutral = properties.get('neutral', False)
         self.concentration = properties.get('concentration', 0.05)
@@ -92,7 +114,7 @@ class Genion:
 
     @launchlogger
     def launch(self) -> int:
-        """Launches the execution of the GROMACS genion module."""
+        """Execute the :class:`Genion <gromacs.genion.Genion>` object."""
         tmp_files = []
 
         # Get local loggers from launchlogger decorator
@@ -170,7 +192,17 @@ class Genion:
         return returncode
 
 
+def genion(input_tpr_path: str, output_gro_path: str, input_top_zip_path: str,
+           output_top_zip_path: str, properties: dict = None, **kwargs) -> int:
+    """Create :class:`Genion <gromacs.genion.Genion>` class and
+        execute the :meth:`launch() <gromacs.genion.Genion.launch>` method."""
+    return Genion(input_tpr_path=input_tpr_path, output_gro_path=output_gro_path,
+                  input_top_zip_path=input_top_zip_path, output_top_zip_path=output_top_zip_path,
+                  properties=properties, **kwargs).launch()
+
+
 def main():
+    """Command line execution of this building block. Please check the command line documentation."""
     parser = argparse.ArgumentParser(description="Wrapper for the GROMACS genion module.",
                                      formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog, width=99999))
     parser.add_argument('-c', '--config', required=False, help="This file can be a YAML file, JSON file or JSON string")
@@ -187,9 +219,9 @@ def main():
     properties = settings.ConfReader(config=config).get_prop_dic()
 
     # Specific call of each building block
-    Genion(input_tpr_path=args.input_tpr_path, output_gro_path=args.output_gro_path,
+    genion(input_tpr_path=args.input_tpr_path, output_gro_path=args.output_gro_path,
            input_top_zip_path=args.input_top_zip_path, output_top_zip_path=args.output_top_zip_path,
-           properties=properties).launch()
+           properties=properties)
 
 
 if __name__ == '__main__':
--- biobb_md/test/unitests/test_gromacs/test_genion.py
+++ biobb_md/test/unitests/test_gromacs/test_genion.py
@@ -1,7 +1,8 @@
 from biobb_common.tools import test_fixtures as fx
-from biobb_md.gromacs.genion import Genion
+from biobb_md.gromacs.genion import genion
 
-class TestGenion():
+
+class TestGenion:
     def setUp(self):
         fx.test_setup(self, 'genion')
 
@@ -10,7 +11,7 @@ class TestGenion():
         fx.test_teardown(self)
 
     def test_genion(self):
-        returncode= Genion(properties=self.properties, **self.paths).launch()
+        returncode = genion(properties=self.properties, **self.paths)
         assert fx.not_empty(self.paths['output_gro_path'])
         assert fx.equal(self.paths['output_gro_path'], self.paths['ref_output_gro_path'])
         assert fx.not_empty(self.paths['output_top_zip_path'])
